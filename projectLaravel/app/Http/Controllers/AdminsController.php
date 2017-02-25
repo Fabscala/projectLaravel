c063f72e48cf1e5b3ec581674d4315c2ffa09bac
Fabien Scalabrin <fscalabrin2@gmail.com>
2017-02-25 21:39:44 +0100

    error message and fix

--- projectLaravel/app/Http/Controllers/AdminsController.php
+++ projectLaravel/app/Http/Controllers/AdminsController.php
@@ -8,9 +8,7 @@ use Illuminate\Http\Request;
 
 class AdminsController extends Controller
 {
-    public function __construct(){
-        $this->middleware('admin');
-    }
+
 
     public function index()
     {
